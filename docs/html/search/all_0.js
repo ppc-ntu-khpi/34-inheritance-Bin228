afd0594fdcf0c029ccfaba8ac73cabcbc6a4df74
Bin228 <55364490+Bin228@users.noreply.github.com>
2021-03-05 15:19:17 +0200

    Add files via upload

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['animal_0',['Animal',['../classdomain_1_1_animal.html#acfbf1405e335fb7887eba1f22cf484c2',1,'domain.Animal.Animal()'],['../classdomain_1_1_animal.html',1,'domain.Animal']]],
+  ['animal_2ejava_1',['Animal.java',['../_animal_8java.html',1,'']]],
+  ['attacks_2',['attacks',['../classdomain_1_1_animal.html#a43868a19d12e2e7a59373677af46322b',1,'domain::Animal']]]
+];
---
+++ docs/html/search/all_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['rest_16',['rest',['../classdomain_1_1_hawk.html#a7cedf95e0c9ca5bd0afeaf820c7d778f',1,'domain::Hawk']]]
+];
